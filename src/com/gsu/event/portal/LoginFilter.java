e5bc97254e37f27baf96af1c142f9d5540b72241
SyedAbedi <sydabedi@gmail.com>
2016-06-12 01:14:54 -0500

    EventPortal

---
+++ src/com/gsu/event/portal/LoginFilter.java
@@ -0,0 +1,51 @@
+package com.gsu.event.portal;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginfilter
+ */
+public class LoginFilter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public String username;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginFilter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		 username = request.getParameter("username");
+		 request.setAttribute("username",username);
+		 if(username.equalsIgnoreCase("mike")){
+			 getServletContext().getRequestDispatcher("/success.jsp").forward(request, response);
+				
+			}
+			else{
+				response.sendRedirect("error.jsp");
+			}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		doGet(request,response);
+	}
+
+}
